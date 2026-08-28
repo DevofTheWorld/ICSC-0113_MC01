cc9b1d61800a834f78d93c72fe2ef176db0d15a7
anthony.dev <anthonylumantao.dev@gmail.com>
2026-08-28 20:22:57 +0800

    Gantt Chart fix

---
+++ src/main/java/app/Launcher.java
@@ -0,0 +1,8 @@
+package app;
+
+    public class Launcher {
+        public static void main(String[] args) {
+            // Points directly to your original JavaFX class
+            app.main(args);
+        }
+    }
